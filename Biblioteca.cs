35f5002fb91919ba97ddfd6dc70475be5c29502c
TheParousia <gabriel11119477@edu.pa.senac.br>
2023-05-10 15:50:49 -0300

    Primeiro Commit

---
+++ Biblioteca.cs
@@ -0,0 +1,6 @@
+class Biblioteca{
+    //Escdreva a sua função alterando o que for nescessário
+    static public double NomeDaFuncao(){
+        return 100;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+        int opt = 0;
+        bool repetir = true;
+        //Insira as variáveis do seu algoritmo aqui
+
+        do{
+            //Apaga o que tem na tela
+            Console.Clear();
+
+            Console.WriteLine("\tMENU");
+            Console.WriteLine("1- Calcula área do losango");
+            Console.WriteLine("2- Área do quadrado");
+            Console.WriteLine("3- Opção 3");
+            Console.WriteLine("4- Opção 4");
+            Console.WriteLine("5- CalculaVolumeEsfera 5");
+            Console.WriteLine("6- Opção 6");
+            Console.WriteLine("4- Sair\n");
+
+            opt = int.Parse(Console.ReadLine());
+
+            
+            switch (opt){
+                case 1:
+                    Console.WriteLine("Opt1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opt3");
+                    break;
+                case 3:
+                    Console.WriteLine("Opt3");
+                    break;
+                case 4:
+                    Console.WriteLine("Opt4");
+                    repetir = false;
+                    break;
+                case 5:
+                    Console.WriteLine("Opt5");
+                    break;
+                default:
+                    Console.WriteLine("Opção Invalida");
+                    break;
+            }
+
+            // Pausa a execução do programa até uma tecla ser apertada
+            Console.ReadKey();
+
+        }while(repetir);
+    }
+    
+}
+
